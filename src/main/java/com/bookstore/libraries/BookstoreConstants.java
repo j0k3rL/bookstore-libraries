3aa17d4817d21877dc3db37512a952d9a1a1271b
Lau <elauso@gmail.com>
2018-03-08 01:58:57 -0300

    Removing some validators

--- src/main/java/com/bookstore/libraries/BookstoreConstants.java
+++ src/main/java/com/bookstore/libraries/BookstoreConstants.java
@@ -9,8 +9,6 @@ public interface BookstoreConstants {
 	public static final String ONLY_NUMBERS_PATTERN = "[^\\d.]";
 	public static final String DEFAULT_DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
 	
-	public static final String CPF_VALIDATION_MESSAGE = "Invalid CPF.";
-	public static final String EMAIL_VALIDATION_MESSAGE = "Invalid E-mail.";
 	public static final String PRODUCT_CODE_VALIDATION_MESSAGE = "Invalid Product Code.";
 	public static final String UNIT_CODE_VALIDATION_MESSAGE = "Invalid Unit Code.";
 	
